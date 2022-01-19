f7423fe489ba08eea6db7acedebc65ff25df1350
samuel-barkley <samuelbarkley2001@gmail.com>
2022-01-19 01:14:55 +0100

    simplified drawSquare and changed drawGrid

--- drawLib.js
+++ drawLib.js
@@ -1,33 +1,40 @@
+let  gridSizeLocal = []
 export function drawGrid(ctx, gridSize) {
+    /*
     for (let i = 0; i < gridSize[0]; i++) {
-        drawLine(ctx, [100 * i, 0], [100 * i, 1000]);
+        drawLine(ctx, [100 * i, 0], [100 * i, gridSize[1]*100]);
     }
 
     for (let j = 0; j < gridSize[1]; j++) {
-        drawLine(ctx, [0, 100 * j], [1000, 100 * j]);
+        drawLine(ctx, [0, 100 * j], [gridSize[0]*100, 100 * j]);
     }
 
-    drawLine(ctx, [gridSize[0] * 100, 0], [gridSize[0] * 100, 1000])
-    drawLine(ctx, [0, gridSize[1] * 100], [1000, gridSize[1] * 100])
+    drawLine(ctx, [gridSize[0] * 100, 0], [gridSize[0] * 100, gridSize[1]*100])
+    drawLine(ctx, [0, gridSize[1] * 100], [gridSize[0]*100, gridSize[1] * 100])
+    */
+    // TODO: grid generation isn't working properly.
+    var w = ctx.canvas.width,
+        h = ctx.canvas.height;
+    //console.log("width: " + w + ", height:" + h)
+    gridSizeLocal = [gridSize[0], gridSize[1]]
+    ctx.beginPath();
+    for (var x = 0; x <= w; x += gridSize[0]) {
+        ctx.moveTo(x - 0.5, 0);      // 0.5 offset so that 1px lines are crisp
+        ctx.lineTo(x - 0.5, h);
+    }
+    for (var y = 0; y <= h; y += gridSize[1]) {
+        ctx.moveTo(0, y - 0.5);
+        ctx.lineTo(w, y - 0.5);
+    }
+    ctx.stroke();
 }
 
 export function drawSquare(ctx, pos, colour = "black") {
-    if (colour == "black") {
-        ctx.strokeStyle = "black";
-        ctx.fillStyle = "black";
-        ctx.fillRect(pos[0] * 100, pos[1] * 100, 100, 100);
-    }
-    else if (colour == "red") {
-        ctx.strokeStyle = "red";
-        ctx.fillStyle = "red";
-        ctx.fillRect(pos[0] * 100, pos[1] * 100, 100, 100);
-    }
-    else if (colour == "grey") {
-        ctx.strokeStyle = "grey";
-        ctx.fillStyle = "grey";
-        ctx.fillRect(pos[0] * 100, pos[1] * 100, 100, 100);
-    }
+    ctx.strokeStyle = colour;
+    ctx.fillStyle = colour;
+    ctx.fillRect(pos[0] * gridSizeLocal[0], pos[1] * gridSizeLocal[1], gridSizeLocal[0], gridSizeLocal[1]);
 
+    // reset to black.
     ctx.fillStyle = "black";
     ctx.strokeStyle = "black";
 }
