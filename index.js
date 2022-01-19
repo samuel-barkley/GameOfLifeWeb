e73c9e573ab77bd532b5f3eee1819dbbfc93867d
samuel-barkley <samuelbarkley2001@gmail.com>
2022-01-19 01:16:04 +0100

    Updated index.js (borked)

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 "use strict"
 import * as d from './drawLib.js';
+import { Cell } from './cell.js';
 
-let Game = {};
+let gameGrid;
+let gameObjects = [];
 let canvas;
 let ctx;
 let stopped = false;
+
+let spaceSizes;
+let nOfSpaces = [30, 30];
+
+let secondsPassed;
+let oldTimeStamp;
+let fps;
 
 let lastRender = 0;
 
@@ -18,33 +27,90 @@ function init() {
 }
 
 function start() {
-    window.requestAnimationFrame(loop)
+    spaceSizes = [Math.floor(window.innerWidth / nOfSpaces[0]), Math.floor(window.innerWidth / nOfSpaces[1])];
+    createGrid();
+    window.requestAnimationFrame(gameLoop)
 }
 
-function update(progress) {
-    console.log(progress)
+function createGrid() {
+    for (let y = 0; y < nOfSpaces[1]; y++) {
+        for (let x = 0; x < nOfSpaces[0]; x++) {
+            gameObjects.push(new Cell(ctx, [x, y]))
+        }
+    }
+    d.drawGrid(ctx, spaceSizes);
 }
 
-function draw(ctx) {
-    d.drawGrid(ctx, [10, 10])
+function gameLoop(timeStamp) {
+
+    // Calculate the number of seconds passed since the last frame
+    secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timeStamp;
+
+    // Calculate fps
+    fps = Math.round(1 / secondsPassed);
+    
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    checkSurrounding();
+
+    // Perform the drawing operation
+    draw();
+
+    // The loop function has reached it's end, keep requesting new frames
+    //setTimeout(() => {
+        window.requestAnimationFrame(() => gameLoop(timeStamp));
+    //}, 100) // The delay will make the game easier to follow
 }
 
-function loop(timestamp) {
-    let progress = timestamp - lastRender
+function draw() {
+    d.blank(ctx);
 
-    update(progress)
-    draw(ctx)
+    for (let i = 0; i < gameObjects.length; i++) {
+        if (gameObjects[i].alive) {
+            d.drawSquare(ctx, [gameObjects[i].pos[0], gameObjects[i].pos[1]], "red");
+        }
+        else {
+            //d.drawSquare(ctx, [gameObjects[i].pos[0], gameObjects[i].pos[1]], "black");
+        }
+    }
+    d.drawGrid(ctx, nOfSpaces)
+    // drawFPS();
+}
 
-    lastRender = timestamp
-    if (!stopped)
-        window.requestAnimationFrame(loop)
+function drawFPS() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 120, 50);
+    ctx.font = '25px Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText("FPS: " + fps, 10, 30);
+}
+
+function checkSurrounding() {
+    // Loop over all cells
+    for (let x = 0; x < nOfSpaces[0]; x++) {
+        for (let y = 0; y < nOfSpaces[1]; y++) {
+            // Count the nearby population
+            let numAlive = isAlive(x - 1, y - 1) + isAlive(x, y - 1) + isAlive(x + 1, y - 1) + isAlive(x - 1, y) + isAlive(x + 1, y) + isAlive(x - 1, y + 1) + isAlive(x, y + 1) + isAlive(x + 1, y + 1);
+            
+        }
+    }
+}
+
+function isAlive(x, y) {
+    if (x < 0 || x >= nOfSpaces[0] || y < 0 || y >= nOfSpaces[1]) {
+        return false;
+    }
+    return gameObjects[gridToIndex(x, y)].alive ? 1 : 0;
+}
+
+function gridToIndex(x, y) {
+    return x + (y * nOfSpaces[0]);
 }
 
 // Handles keypresses.
-function keyPressed(e)
-{
-    switch(e.keyCode)
-    {
+function keyPressed(e) {
+    switch (e.keyCode) {
         case 27:
             stopUpdates()
             break;
@@ -52,7 +118,6 @@ function keyPressed(e)
 }
 
 // Stops the update cycle of the application.
-function stopUpdates()
-{
+function stopUpdates() {
     stopped = true;
 }
